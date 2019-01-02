32a9d80350e1d5054a36b677e7aea799457f35e1
thealexvarga <thealexvarga@gmail.com>
2019-01-02 15:23:28 -0500

    more efficient AND; playing with some arbitrary constraints

--- 5value.py
+++ 5value.py
@@ -32,8 +32,8 @@ def FALSE(p, name=None): return Not(TRUE(p, name=name))
 def NOT(p, name=None): return ind(N, p, fs=fs, name=name or "-%s" % p)
 def IMP(p, q, name=None): return ind(I, p, q, fs=fs, name=name or "(%s->%s)" % (p, q))
 def OR(p, q, name=None):  return IMP(IMP(p, q), q, name=name or "(%s||%s)" % (p, q))
-def AND(p, q, name=None): return NOT(OR(NOT(p), NOT(q)), name=name or "(%s&&%s)" % (p, q))
-# def AND(p, q, name=None): return NOT(IMP(p, NOT(q)), name=name or "(%s&&%s)" % (p, q))
+# def AND(p, q, name=None): return NOT(OR(NOT(p), NOT(q)), name=name or "(%s&&%s)" % (p, q))
+def AND(p, q, name=None): return NOT(IMP(p, NOT(q)), name=name or "(%s&&%s)" % (p, q))
 
 
 ### Formulas ###
@@ -41,10 +41,6 @@ def AND(p, q, name=None): return NOT(OR(NOT(p), NOT(q)), name=name or "(%s&&%s)"
 # in range
 fs += [ And(0 <= NOT(i), NOT(i) < n) for i in range(n) ]
 fs += [ And(0 <= IMP(i, j), IMP(i, j) < n) for i in range(n) for j in range(n) ]
-
-# assume wlog
-fs += [ NOT(i) >= NOT(i+1) for i in range(n-1) ]
-fs.append(D[-1])
 
 # not true isn't true
 fs += [ Implies(TRUE(i), FALSE(NOT(i))) for i in range(n) ]
@@ -69,13 +65,32 @@ fs += [ Implies(And(TRUE(i), TRUE(j)), TRUE(k)) == Implies(TRUE(AND(i, j)), TRUE
 # Right Conj for atoms
 fs += [ TRUE(AND(i, j)) == And(TRUE(i), TRUE(j)) for i in range(n) for j in range(5) ]
 
+### Domain Restriction ###
+
+# assume wlog
+fs += [ NOT(i) >= NOT(i+1) for i in range(n-1) ]
+fs.append(D[-1])
+
+# specify negation
+fs += [NOT(i) 	 == n-1-i 	for i in range(n)]
+# specify truth designation
+# fs += [TRUE(i)   == (i>n-2) for i in range(n)]
+# specify edges
+fs += [IMP(0,i)  == n-1   	for i in range(n)]
+fs += [IMP(-1,i) == i 	  	for i in range(n)]
+fs += [IMP(i,0)  == n-1-i 	for i in range(n)]
+fs += [IMP(i,-1) == n-1   	for i in range(n)]
+# monotonicity
+fs += [IMP(i,j) <= IMP(i,j+1) for i in range(n) for j in range(n-1)]
+fs += [IMP(i,j) >= IMP(i+1,j) for i in range(n-1) for j in range(n)]
+
 ### Solve ###
 
 # pp(fs)
 pp("%s Formulas\n" % len(fs))
 
 M = 50			# number of models to generate
-V = N + D 		# set of variables to avoid full repeats of
+V = None#N + D 		# set of variables to avoid full repeats of
 
 for i, m in enumerate(get_models(fs, M, V)):
 	print("--- Model %s ---" % (i+1))
